7305770a201b8e537186dea1b377938411f90fd9
Mishra <r78mishra@gmail.com>
2022-05-19 17:08:17 +0530

    Third commit

--- src/java/git/JavaGit.java
+++ src/java/git/JavaGit.java
@@ -16,6 +16,7 @@ public class JavaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome Javagit");
     }
     
 }
